c3f686310535ce912ae738cf99989f74b579edb9
陈 <iloveweb1996@gmail.com>
2022-02-21 22:50:54 +0800

    feat: 编辑文件名 修改打包脚本

--- gitHook/push.sh
+++ gitHook/push.sh
@@ -1,12 +1,18 @@
 #! /bin/bash
 cd /root/woerk/cms_cms || exit
 rm -rf /root/woerk/cms_cms/*
+echo "clone"
 git clone -b developer https://git.tiancai.work/174/174-2.git
 cd /root/woerk/cms_cms/174-2 || exit
+echo "安装包"
 yarn install
-yarn build:test
+echo "build"
+yarn build:test || exit
+if [ -d "/root/woerk/dist/" ]; then
+    echo "删除dist"
+    rm -rf /root/woerk/dist
+fi
 cp -r /root/woerk/cms_cms/174-2/dist /root/woerk
 rm -rf /root/woerk/cms_cms/*
-rm -rf /root/woerk/dist || exit
 
 
